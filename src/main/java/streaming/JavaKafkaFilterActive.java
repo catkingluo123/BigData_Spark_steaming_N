78e12441dc2df4abd9871bab0f3ba2de3fb97489
chyeers <1172047316@qq.com>
2016-11-20 11:59:42 +0800

    New changelist

--- src/main/java/streaming/JavaKafkaFilterActive.java
+++ src/main/java/streaming/JavaKafkaFilterActive.java
@@ -10,10 +10,7 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.State;
 import org.apache.spark.streaming.StateSpec;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaMapWithStateDStream;
-import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import redis.RedisClientPool;
 import redis.clients.jedis.Jedis;
@@ -60,69 +57,71 @@ public class JavaKafkaFilterActive {
             }
         });
 
-        JavaMapWithStateDStream<Tuple2<String, String>, Integer, Integer, Tuple2<Tuple2<String ,String>, Integer>> activeStream =
-                lines.flatMapToPair(new PairFlatMapFunction<String, String, String>() {
+        JavaPairDStream<String ,String> listStream = lines.flatMapToPair(new PairFlatMapFunction<String, String, String>() {
+            @Override
+            public Iterator<Tuple2<String, String>> call(String x) throws Exception {
+                String[] words = SPACE.split(x);
+                List<Tuple2<String, String>> list = new ArrayList<Tuple2<String, String>>();
+                String form_phone = null;
+                String to_phone = null;
+                String day = null;
+
+                if (words.length > 3) {
+                    day = words[0].substring(0, 8);
+                    form_phone = words[2].substring(2, 13);
+                    to_phone = words[3].substring(2, 13);
+                }
+
+                list.add(new Tuple2<String, String>(form_phone, day));
+                list.add(new Tuple2<String, String>(to_phone, day));
+
+                return list.iterator();
+            }
+        });
+
+        JavaMapWithStateDStream<Tuple2<String, String>, Integer, Integer,
+                Tuple2<Tuple2<String ,String>, Integer>>
+                activeStream = listStream.filter(new Function<Tuple2<String, String>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<String, String> v1) throws Exception {
+                String phone = v1._1();
+                String day = v1._2();
+                Jedis jedis = RedisClientPool.getInstance().getPool().getResource();
+                if (jedis.exists(phone)) {
+                    String reg_time = jedis.get(phone);
+                    int timeTest = day.compareTo(reg_time);
+                    if (timeTest != -1) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }).mapToPair(new PairFunction<Tuple2<String,String>, Tuple2<String ,String>, Integer>() {
+            @Override
+            public Tuple2<Tuple2<String, String>, Integer> call(Tuple2<String, String> stringStringTuple2) throws Exception {
+                String day = stringStringTuple2._2();
+                Tuple2<String,String> account = new Tuple2<String, String>(stringStringTuple2._1(),day);
+                return new Tuple2<Tuple2<String, String>, Integer>(account ,1);
+            }
+        }).mapWithState(StateSpec.function(
+                new Function3<Tuple2<String, String>, Optional<Integer>, State<Integer>, Tuple2<Tuple2<String, String>, Integer>>() {
                     @Override
-                    public Iterator<Tuple2<String, String>> call(String x) throws Exception {
-                        String[] words = SPACE.split(x);
-                        List<Tuple2<String, String>> list = new ArrayList<Tuple2<String, String>>();
-                        String form_phone = null;
-                        String to_phone = null;
-                        String day = null;
-
-                        logger.warn(words.toString());
-
-                        if (words.length > 3) {
-                            day = words[0].substring(0,8);
-                            form_phone = words[2].substring(2,13);
-                            to_phone = words[3].substring(2,13);
+                    public Tuple2<Tuple2<String, String>, Integer> call(Tuple2<String, String> v1, Optional<Integer> one, State<Integer> state) throws Exception {
+                        int old = state.exists() ? state.get() : 0;
+                        if (old == 0) {
+                            // redis put
+                            logger.warn("day:" + v1._2() + " phone:" + v1._1());
+                            Jedis jedis = RedisClientPool.getInstance().getPool().getResource();
+                            jedis.pfadd(v1._2(), v1._1());
                         }
-
-                        list.add(new Tuple2<String, String>(form_phone, day));
-                        list.add(new Tuple2<String, String>(to_phone, day));
-
-                        return list.iterator();
+                        int sum = one.or(0) + old;
+                        Tuple2<Tuple2<String, String>, Integer> output = new Tuple2<Tuple2<String, String>, Integer>(v1, sum);
+                        state.update(sum);
+                        return output;
                     }
-                }).filter(new Function<Tuple2<String, String>, Boolean>() {
-                    @Override
-                    public Boolean call(Tuple2<String, String> v1) throws Exception {
-                        String phone = v1._1();
-                        String day = v1._2();
-                        Jedis jedis = RedisClientPool.getInstance().getPool().getResource();
-                        if(jedis.exists(phone)){
-                            String reg_time = jedis.get(phone);
-                            int timeTest = day.compareTo(reg_time);
-                            if(timeTest != -1){
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-                }).mapToPair(new PairFunction<Tuple2<String,String>, Tuple2<String ,String>, Integer>() {
-                    @Override
-                    public Tuple2<Tuple2<String, String>, Integer> call(Tuple2<String, String> stringStringTuple2) throws Exception {
-                        String day = stringStringTuple2._2();
-                        Tuple2<String,String> account = new Tuple2<String, String>(stringStringTuple2._1(),day);
-                        return new Tuple2<Tuple2<String, String>, Integer>(account ,1);
-                    }
-                }).mapWithState(StateSpec.function(
-                        new Function3<Tuple2<String, String>, Optional<Integer>, State<Integer>, Tuple2<Tuple2<String, String>, Integer>>() {
-                            @Override
-                            public Tuple2<Tuple2<String, String>, Integer> call(Tuple2<String, String> v1, Optional<Integer> one, State<Integer> state) throws Exception {
-                                int old = state.exists() ? state.get() : 0;
-                                if (old == 0) {
-                                    // redis put
-                                    logger.warn("day:" + v1._2() + " phone:" + v1._1());
-                                    Jedis jedis = RedisClientPool.getInstance().getPool().getResource();
-                                    jedis.pfadd(v1._2(), v1._1());
-                                }
-                                int sum = one.or(0) + old;
-                                Tuple2<Tuple2<String, String>, Integer> output = new Tuple2<Tuple2<String, String>, Integer>(v1, sum);
-                                state.update(sum);
-                                return output;
-                            }
-                        }));
+                }));
 
+        listStream.print();
         activeStream.print();
         jssc.start();
         jssc.awaitTermination();
